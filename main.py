3815f8b91cc5a252fad42c9f680230e34a2f5aa0
Rostislav Stogov <rost@stogoff.ru>
2020-09-02 11:35:40 +0300

    code inspection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from kivy.uix.gridlayout import GridLayout
-from kivy.config import  ConfigParser
+from kivy.config import ConfigParser
 from kivymd.theming import ThemeManager
 from kivymd.app import MDApp
 from kivy.logger import Logger
@@ -10,9 +10,10 @@ import ast
 import time
 
 os.environ['SSL_CERT_FILE'] = certifi.where()
-#from kivy.core.window import Window
-#Window.size = (480, 853)
+# from kivy.core.window import Window
+# Window.size = (480, 853)
 __version__ = '0.3.7'
+
 
 def get_rates():
     url = 'https://openexchangerates.org/api/latest.json?app_id=43d720b184b24b0d8157da339f12f17c'
@@ -38,16 +39,14 @@ class Container(GridLayout):
             self.app.user_data['rates'] = self.rates
             self.app.config.set('General', 'user_data', self.app.user_data)
             self.app.config.write()
-
             self.timelabel.text = time.strftime("%d.%m.%Y, %H:%M:%S",
                                                 time.localtime(self.app.user_data['update']))
-
         except:
-            rates = {}
-            Logger.exception('Something happened!')
+            self.rates = {}
+            Logger.exception('Cannot get rates from site!')
 
     def calculate(self):
-        #self.app =
+        # self.app =
         self.app.user_data = ast.literal_eval(
             self.app.config.get('General', 'user_data'))
         if 'update' in self.app.user_data.keys() and (time.time() - self.app.user_data['update'] < 1800):
@@ -56,14 +55,14 @@ class Container(GridLayout):
             self.update_rates()
         try:
             amount1 = float(self.amount1.text)
-        except:
+        except TypeError:
             amount1 = 0
-        k = self.rates[self.curr2.text]/self.rates[self.curr1.text]
+        k = self.rates[self.curr2.text] / self.rates[self.curr1.text]
         if self.curr2.text in ['BTC']:
             fmt = "{:.8f}"
         else:
             fmt = "{:.2f}"
-        self.amount2.text = fmt.format(amount1*k)
+        self.amount2.text = fmt.format(amount1 * k)
         self.app.config.set('General', 'currencies', [self.curr1.text, self.curr2.text])
         self.app.config.write()
 
@@ -87,8 +86,8 @@ class CurrApp(MDApp):
     theme_man = ThemeManager()
     title = "Currency calc"
 
-    def __init__(self, **kvargs):
-        super(CurrApp, self).__init__(**kvargs)
+    def __init__(self, **kwargs):
+        super(CurrApp, self).__init__(**kwargs)
         self.config = ConfigParser()
         self.user_data = {}
         print(self.config)
@@ -103,8 +102,7 @@ class CurrApp(MDApp):
         self.user_data = ast.literal_eval(self.config.get(
             'General', 'user_data'))
 
-
-    def get_application_config(self):
+    def get_application_config(self, **kwargs):
         return super(CurrApp, self).get_application_config(
             '{}/%(appname)s.ini'.format(self.directory))
 
